25208a326abbcd95c6591256ab9a0faec63d7e29
Nandushangrila <nivedr06@gmail.com>
2025-07-09 13:27:18 -0500

    initial commit

---
+++ name conversion.py
@@ -0,0 +1,15 @@
+name="congratulations"
+print("congratulations")
+Uppercase_String=name.upper()
+print(Uppercase_String)
+
+name="congratulations"
+length=len(name)
+print("Length of the name variable is",length)
+name2=" on winning"
+print("New Word:",name+name2)
+New_Word=name+name2
+Length=len(New_Word)
+print("Length of the name variable is",Length)
+rev_name=New_Word[: : -1]
+print(rev_name)
