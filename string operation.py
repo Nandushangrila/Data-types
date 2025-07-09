5d96cdab02afbdd95b28ea9df04f9ca99340a4dd
Nandushangrila <nivedr06@gmail.com>
2025-07-09 12:40:01 -0500

    initial commit

---
+++ string operation.py
@@ -0,0 +1,8 @@
+name="Volcano"
+Length=len(name)
+print("Length of the name variable is",Length)
+name2="Eruption"
+print("New Word:",name+name2)
+New_Word=name+name2
+rev_name=New_Word[: : -1]
+print(rev_name)
---
+++ type conversion.py
@@ -0,0 +1,5 @@
+age=12
+print(type(age))
+age=float(age)
+print(type(age))
+print("My age is ",age)
